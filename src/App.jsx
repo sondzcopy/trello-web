db302de3c20dd98c232dba08261ecaa2238449ce
sondzcopy <nguyenson22abs@gmail.com>
2025-12-18 18:03:15 +0700

    dark_light

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,27 @@ import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
 import HomeIcon from '@mui/icons-material/Home'
 import { pink } from '@mui/material/colors'
 import Typography from '@mui/material/Typography'
+
+import { useColorScheme } from '@mui/material/styles'
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  
+  return (
+    <Button
+      variant="outlined"
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}>
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
 function App() {
   return (
     <>
+      <ModeToggle />  
+      <hr />
       <div>Nguyễn Hồng Sơn</div>
       <Typography variant="body2" color="text.secondary">Text Typograply</Typography>
       <Button variant="text">Text</Button>
