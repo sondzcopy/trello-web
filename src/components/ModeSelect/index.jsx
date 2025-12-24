66fc554f7f0a52af35c328acf5d7b6a414b84ef5
sondzcopy <nguyenson22abs@gmail.com>
2025-12-24 14:48:16 +0700

    appbar3

--- src/components/ModeSelect/index.jsx
+++ src/components/ModeSelect/index.jsx
@@ -16,7 +16,7 @@ function ModeSelect() {
   }
 
   return (
-    <FormControl size="small">
+    <FormControl size="small" sx={{ color: 'primary.main' }}>
       <InputLabel id="label-select-dark-light-mode">Mode</InputLabel>
       <Select
         labelId="label-select-dark-light-mode"
