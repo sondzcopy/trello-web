f6883111712364384cb56c38a7ca16cd49c9ff19
sondzcopy <nguyenson22abs@gmail.com>
2025-12-24 22:39:03 +0700

    trello_board_content2_lai

--- src/pages/Boards/BoardContent/ListColumns/ListColumns.jsx
+++ src/pages/Boards/BoardContent/ListColumns/ListColumns.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Box from '@mui/material/Box'
 import Column from './Column/Column'
-import { Button } from '@mui/material'
+import Button from '@mui/material/Button'
 import NoteAddIcon from '@mui/icons-material/NoteAdd'
 function ListColumns() {
   return (
