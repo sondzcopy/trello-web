16dd7698025f6c9838c5d5069ae460706e299c78
sondzcopy <nguyenson22abs@gmail.com>
2026-01-10 15:06:20 +0700

    Fix BUILD_MOVE dev and prod

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,12 @@
-export const API_ROOT = 'http://localhost:8017'
+let apiRoot = ''
+
+console.log('import.mete', import.meta.env)
+console.log('process.env', process.env)
+
+if (process.env.BUILD_MODE === 'dev') {
+  apiRoot = 'http://localhost:8017'
+}
+if (process.env.BUILD_MODE === 'production') {
+  apiRoot = 'https://trello-api-wt7a.onrender.com'
+}
+export const API_ROOT = apiRoot
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 import svgr from 'vite-plugin-svgr'
 // https://vite.dev/config/
 export default defineConfig({
+  define: {
+    'process.env': process.env
+  },
   plugins: [
     react(),
     svgr()
