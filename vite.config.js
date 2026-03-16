4c8e1771a1748658c8b5e5605baeeb9834ea0b29
sondzcopy <nguyenson22abs@gmail.com>
2026-03-16 13:33:31 +0700

    Fix vercel

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import svgr from 'vite-plugin-svgr'
+import path from 'path' // Thêm dòng này
+
 // https://vite.dev/config/
 export default defineConfig({
+  // 1. Đảm bảo base là đường dẫn tuyệt đối
+  base: '/',
+
   define: {
     'process.env': process.env
   },
@@ -12,7 +17,8 @@ export default defineConfig({
   ],
   resolve: {
     alias: [
-      { find: '~', replacement: '/src' }
+      // 2. Sử dụng path.resolve để đảm bảo alias luôn đúng khi build
+      { find: '~', replacement: path.resolve(__dirname, './src') }
     ]
   }
 })
